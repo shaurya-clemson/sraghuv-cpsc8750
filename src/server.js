3f78ad7b9aa2a25bb76693c0b073aba39a3843a7
shaurya raghuvanshi <sraghuv@clemson.edu>
2022-10-25 23:48:26 -0400

    adding all Dynamic Website assignment related updates

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // use the express library
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 // create a new server application
 const app = express();
@@ -11,9 +12,44 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-// The main page of our website
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+// set view engine to ejs
+app.set('view engine', 'ejs');
+
+let nextVisitorId = 1;
+
+
+//The new main page
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let lastVisited = req.cookies.visited;
+  let currentVisitorId = req.cookies.visitorId;
+  
+  res.cookie('visited', Date.now().toString());
+
+  if(!currentVisitorId){
+    currentVisitorId = ++nextVisitorId;
+    res.cookie('visitorId', currentVisitorId);
+  }
+
+  if(lastVisited){
+    lastVisited = Math.floor((Date.now() - lastVisited) / 1000);
+  }
+  else{
+    lastVisited = null;
+  }  
+
+  console.log(req.cookies.visited);
+
+  res.render('welcome', {
+    name: req.query.name || "World",
+    currentVisitDate: new Date().toLocaleString(),
+    currentVisitorId: currentVisitorId,
+    timeSinceLastVisit: lastVisited  
+  });
 });
 
 // Start listening for network connections
