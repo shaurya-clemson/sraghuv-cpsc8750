913b991ca1d8651713943bfc5147c0b0a6ae5c77
shaurya raghuvanshi <sraghuv@clemson.edu>
2022-11-08 19:30:54 -0500

    Something broke while uploading last time, reuploading the fixed code

--- src/server.js
+++ src/server.js
@@ -12,16 +12,17 @@ const app = express();
 // first, that is for when this is used on the real
 // world wide web).
 const port = process.env.PORT || 3000;
-
 app.use(express.static('public'));
 
+// Start listening for network connections
+app.listen(port);
+
+// Printout for readability
+console.log("Server Started!");
 app.use(cookieParser());
 
-// set view engine to ejs
-app.set('view engine', 'ejs');
 
 let nextVisitorId = 1;
-
 
 //The new main page
 app.get('/', (req, res) => {
@@ -52,50 +53,46 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get("/trivia", async (req, res) => {
-  // fetch the data
-  const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+app.get('/trivia', async (req, res) => {
+    // fetch the data
+    const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
 
-  // fail if bad response
-  if (!response.ok) {
-    res.status(500);
-    res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
-    return;
-  }
+    // fail if bad response
+    if (!response.ok) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+        return;
+    }
 
-  // interpret the body as json
-  const content = await response.json();
+    // interpret the body as json
+    const content = await response.json();
 
-  /*// fail if db failed
-  if (data.response_code !== 0) {
-    res.status(500);
-    res.send(`Open Trivia Database failed with internal response code ${data.response_code}`);
-    return;*/
+    // TODO: make proper html
+    const format = JSON.stringify(content, 2);
 
-  const format = JSON.stringify(content,2);
+    const correctAnswer = content.results[0].correct_answer;
+    const answers = content.results[0].incorrect_answers;
+    answers.push(correctAnswer);
 
-  const correctAnswer = content.results[0].correct_answer;
-  const answers = content.results[0].incorrect_answers;
-  answers.push(correctAnswer);
+     // Randomize Answers Array
+    answers.sort(() => Math.random() - 0.5);
 
-  const answerLinks = answers.map(answer => {
-    return `<a href="javascript:alert('${
-      answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
-      }')">${answer}</a>`
-});
+    const answerLinks = answers.map(answer => {
+        return `<a href="javascript:alert('${
+        answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+        }')">${answer}</a>`
+    });
 
-res.render('trivia',{
-  category:  content.results[0].category,
-  difficulty: content.results[0].difficulty,
-  question: content.results[0].question,
-  answers: answers,
-  answerLinks: answerLinks,
-});
-  
-});
+    res.render(
+        'trivia', {
+            category: content.results[0].category,
+            difficulty: content.results[0].difficulty,
+            question: content.results[0].question,
+            answers: answers,
+            linksToAnswers: answerLinks,
+        }
+    );
+})
 
-// Start listening for network connections
-app.listen(port);
-
-// Printout for readability
-console.log("Server Started!");
+// set the view engine to ejs
+app.set('view engine', 'ejs');
